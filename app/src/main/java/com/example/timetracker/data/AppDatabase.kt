c9473301d9efaea4245cbe935245e031f1204e2d
Ardemon96 <54725562+Ardemon96@users.noreply.github.com>
2026-09-09 02:45:37 +0300

    19

--- app/src/main/java/com/example/timetracker/data/AppDatabase.kt
+++ app/src/main/java/com/example/timetracker/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ActivityLog::class, CategoryEntity::class], version = 2, exportSchema = false)
+@Database(entities = [ActivityLog::class, CategoryEntity::class], version = 3, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun activityDao(): ActivityDao
--- app/src/main/java/com/example/timetracker/data/CategoryEntity.kt
+++ app/src/main/java/com/example/timetracker/data/CategoryEntity.kt
@@ -3,7 +3,7 @@ package com.example.timetracker.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "category_entity")
+@Entity(tableName = "categories")
 data class CategoryEntity(
     @PrimaryKey val name: String
 )
