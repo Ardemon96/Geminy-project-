0f6f58a3dc8841b11acce3cf21ac3ee0daf225d7
Ardemon96 <54725562+Ardemon96@users.noreply.github.com>
2026-09-08 23:38:40 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,5 @@
-buildscript {
-    ext {
-        // Актуальные стабильные версии
-        agp_version = '8.4.2'
-        kotlin_version = '2.0.0'
-    }
-    dependencies {
-        classpath "com.android.tools.build:gradle:$agp_version"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-    }
-}
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id 'com.android.application' version '8.4.2' apply false
-    id 'org.jetbrains.kotlin.android' version '2.0.0' apply false
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
