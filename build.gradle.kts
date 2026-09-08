b6ed3befac98a971296b57e9a626b50144680b47
Ardemon96 <54725562+Ardemon96@users.noreply.github.com>
2026-09-08 23:27:46 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
+buildscript {
+    ext {
+        // Актуальные стабильные версии
+        agp_version = '8.4.2'
+        kotlin_version = '2.0.0'
+    }
+    dependencies {
+        classpath "com.android.tools.build:gradle:$agp_version"
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
+    id 'com.android.application' version '8.4.2' apply false
+    id 'org.jetbrains.kotlin.android' version '2.0.0' apply false
 }
