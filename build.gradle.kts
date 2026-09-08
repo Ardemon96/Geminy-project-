42b8f1f076ae601ca66b76074f8f375f9379547c
Ardemon96 <54725562+Ardemon96@users.noreply.github.com>
2026-09-09 00:58:07 +0300

    2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
-// Top-level build file
+// Корневой build.gradle.kts
 plugins {
-    id("com.android.application") version "8.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
